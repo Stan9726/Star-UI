564026e1a7269b64614445e81783a761c6443348
Stan9726 <Stan9726@users.noreply.github.com>
2021-05-01 12:02:25 +0800

    Button

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,82 @@
 <template>
-	<div id="app"></div>
+	<div id="app">
+		<st-button>按钮</st-button>
+		<st-button type="primary">登录</st-button>
+		<st-button type="danger">删除</st-button>
+		<st-button type="info">取消</st-button>
+		<st-button type="success">确定</st-button>
+		<st-button type="warning">提示</st-button>
+
+		<st-button plain>按钮</st-button>
+		<st-button plain
+							 type="primary">登录</st-button>
+		<st-button plain
+							 type="danger">删除</st-button>
+		<st-button plain
+							 type="info">取消</st-button>
+		<st-button plain
+							 type="success">确定</st-button>
+		<st-button plain
+							 type="warning">提示</st-button>
+
+		<st-button round>按钮</st-button>
+		<st-button round
+							 type="primary">登录</st-button>
+		<st-button round
+							 type="danger">删除</st-button>
+		<st-button round
+							 type="info">取消</st-button>
+		<st-button round
+							 type="success">确定</st-button>
+		<st-button round
+							 type="warning">提示</st-button>
+
+		<st-button circle>按钮</st-button>
+		<st-button circle
+							 type="primary">登录</st-button>
+		<st-button circle
+							 type="danger">删除</st-button>
+		<st-button circle
+							 type="info">取消</st-button>
+		<st-button circle
+							 type="success">确定</st-button>
+		<st-button circle
+							 type="warning">提示</st-button>
+
+		<st-button icon="appreciate"></st-button>
+		<st-button icon="wang"
+							 type="primary">登录</st-button>
+		<st-button icon="delete"
+							 type="danger">删除</st-button>
+		<st-button icon="close"
+							 type="info">取消</st-button>
+		<st-button icon="check"
+							 type="success">确定</st-button>
+		<st-button icon="info"
+							 type="warning">提示</st-button>
+
+		<st-button @click="showInfo">按钮</st-button>
+
+		<st-button disabled>按钮</st-button>
+	</div>
 </template>
 
 <script>
 export default {
 	name: 'App',
-	data() {
-		return {}
+	methods: {
+		showInfo() {
+			console.log('点了一下！')
+		}
 	}
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+#app {
+	.st-button {
+		margin-right: 30px;
+	}
+}
+</style>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import StarUI from '../modules/Star-UI'
+import { StButton } from '../modules/Star-UI'
+import './assets/fonts/iconfont.css'
 
 Vue.config.productionTip = false
+
+// Vue.use(StarUI, {
+// 	components: ['StButton']
+// })
+
+Vue.use(StButton)
 
 new Vue({
 	render: h => h(App)
