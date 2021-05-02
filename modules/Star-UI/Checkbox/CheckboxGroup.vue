d1090430f402047bd34f3b13e700a971c777f630
Stan9726 <Stan9726@users.noreply.github.com>
2021-05-02 22:22:48 +0800

    Checkbox

---
+++ modules/Star-UI/Checkbox/CheckboxGroup.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="st-checkbox-group">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'StCheckboxGroup',
+	props: {
+		value: {
+			type: Array
+		}
+	},
+	provide() {
+		return {
+			CheckboxGroup: this
+		}
+	}
+}
+</script>
+
+<style scoped lang="scss">
+</style>
